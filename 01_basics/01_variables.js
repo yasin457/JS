1dd6a9c3cedfe32f70b5f136eb93a8a4987478fe
Yasin Khan <130445662+yasin457@users.noreply.github.com>
2025-08-28 20:31:54 +0000

    Update variable assignments and clarify accountId restriction

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,6 +3,10 @@ let accountEmail = "Yasin@google.com";
 var accountPassword = "32334";
 accountCity = "Lahore";
 
-// accountId = 2;
-
+// accountId = 2;  NOT Allowed
+accountEmail =  "yk@yk.com"
+accountPassword = "2323";
+accountCity = "Islamabad";
 console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
