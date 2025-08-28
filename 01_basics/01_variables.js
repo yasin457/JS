616f6d6c30177f25a43ea13ccc78ebd265a08f06
Yasin Khan <130445662+yasin457@users.noreply.github.com>
2025-08-28 20:40:53 +0000

    Refactor variable declarations and enhance comments on usage

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,12 +1,20 @@
 const accountId = 1234;
 let accountEmail = "Yasin@google.com";
 var accountPassword = "32334";
-accountCity = "Lahore";
+accountCity = "Lahore";   //Not recommended completey avoid it
 
+let accountState; // prints the value undefined
 // accountId = 2;  NOT Allowed
 accountEmail =  "yk@yk.com"
 accountPassword = "2323";
 accountCity = "Islamabad";
 console.log(accountId);
+
+
+/* 
+Perfer Not to use var.
+because of issue in block scope and functional scope
+
+*/
 
 console.table([accountId, accountEmail, accountPassword, accountCity]);
