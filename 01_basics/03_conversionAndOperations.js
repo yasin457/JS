ee8d6be0d4d6f156dab6dfadfccdf9053e49ce87
Yasin Khan <130445662+yasin457@users.noreply.github.com>
2025-08-31 07:43:33 +0000

    Refactor conversion and operations examples; comment out console logs and enhance code clarity in 03_conversionAndOperations.js

--- 01_basics/03_conversionAndOperations.js
+++ 01_basics/03_conversionAndOperations.js
@@ -67,4 +67,39 @@ let name = "Yasin"
 
 let greetSome = greet+name;
 
-console.log(greetSome);
+// console.log(greetSome);
+
+// console.log("1" + 2); //12
+
+// console.log(1 + "2"); //12
+
+// console.log("1"+"2"); //12
+// console.log("1" +2 + 2); //122
+// console.log(2 + 2 + "1"); //41
+
+// console.log("Yasin" - "a"); // NaN
+// console.log("Yasin" * "a"); // NaN
+// console.log("Yasin" / "a"); // NaN
+
+// console.log("4" - "2");
+
+// console.log(+true);
+// console.log(true+);
+// console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2+2
+// console.log(num1, num2, num3);
+
+num1 += 2  // num1 = num1 + 2
+// console.log(num1);
+
+let gameCounter = 100;
+gameCounter++  // gameCounter = gameCounter + 1
+++gameCounter  // gameCounter = gameCounter + 1
+// console.log(gameCounter);
+
+
+// Link to Study More on conversion and operations
+//https://tc39.es/ecma262/#sec-toprimitive
+//https://tc39.es/ecma262/#sec-tonumber
