c653503a1d8c331009f2d3d94f4df8ec781abb08
Yasin Khan <130445662+yasin457@users.noreply.github.com>
2025-09-02 05:53:01 +0000

    Add initial implementation of data types summary in datatypes_summary.js

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,70 @@
+
+// Primitive Data Types
+
+//  7 types;    String, Number, Boolean, null, undefined,Symbol, BigInt, 
+
+
+
+
+// Is JavaScript a dynamically typed or statically typed language? Explain your answer with examples.
+
+const score = 100; // Number
+const scoreValue = 100.3; // Number
+
+const isLoggedIn = false; // Boolean
+
+const outsideTemp = null; // null
+
+let userEmail; // undefined
+
+const id = Symbol("123"); // Symbol
+
+const anotherId = Symbol("123"); // Symbol
+
+// console.log(id === anotherId); // false
+
+const bigNumber = 1234567890123456789012345678901234567890n; // BigInt
+
+// Reference (Non primitive)
+
+// Arrays, Objects, Functions, 
+
+
+const heros = ["Shaktiman", "Naagraj", "Doga", "Bhokal"]; // Array
+
+const myObj = {
+  name: "Yasin",
+  age: 22,
+  isLoggedIn: false,
+}; // Object
+
+const myFunction = function () {
+    console.log("Hello World!");
+    
+}
+
+console.log(typeof bigNumber);
+
+
+// Object (native and does not implement [[Call]]) → "object"
+typeof {};        // "object"
+typeof [];        // "object" (array bhi object hai)
+typeof new Date(); // "object"
+
+
+
+
+// Object (native or host and does implement [[Call]]) → "function"
+
+function greet() {}
+typeof greet; // "function"
+
+const sayHi = () => {};
+typeof sayHi; // "function"
+
+
+
+// Object (host and does not implement [[Call]]) → Implementation-defined
+
+typeof document; // "object"
+typeof alert;    // "function" (kyunki ye callable hai)
