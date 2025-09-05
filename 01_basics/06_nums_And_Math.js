c361642be9fccfe011692b4434a0e88dac4e9c1a
Yasin Khan <130445662+yasin457@users.noreply.github.com>
2025-09-05 07:27:15 +0000

    Numbers And Math in JavaScript

---
+++ 01_basics/06_nums_And_Math.js
@@ -0,0 +1,16 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.8956
+console.log(otherNumber.toPrecision(3))
+
+
+const hundreds = 100000;
+console.log(hundreds.toLocaleString());
